a421957fd0243375956bbee966e36cdf3e3e225d
kulikov <kulikov@bringo247.ru>
2016-12-05 18:45:57 +0300

    testing serviceWorker

--- src/MyserviceWorker.js
+++ src/MyserviceWorker.js
@@ -1,5 +1,23 @@
-self.addEventListener('install', event => {
-   console.log('qqqq');
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('the-magic-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/static/',
+                '/static/bundle.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+   console.log('sd');
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
 
 self.addEventListener('activate', event => {
